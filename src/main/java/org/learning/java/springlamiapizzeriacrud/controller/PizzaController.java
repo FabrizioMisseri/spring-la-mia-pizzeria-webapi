808fcb38d501a8b7b0aaefaad8409216294f5eb0
FabrizioMisseri <misserifabrizio@gmail.com>
2023-03-30 14:53:54 +0200

    add show

--- src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -4,10 +4,13 @@ package org.learning.java.springlamiapizzeriacrud.controller;
 import org.learning.java.springlamiapizzeriacrud.model.Pizza;
 import org.learning.java.springlamiapizzeriacrud.repository.PizzaRpository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -23,7 +26,15 @@ public class PizzaController {
 
         List<Pizza> pizzas = pizzaRpository.findAll();
         model.addAttribute("allPizza", pizzas);
-        return "pizzas/index";
+        return "/pizzas/index";
+    }
+
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Pizza pizza = pizzaRpository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("pizza", pizza);
+        return "/pizzas/show";
     }
 
 }
