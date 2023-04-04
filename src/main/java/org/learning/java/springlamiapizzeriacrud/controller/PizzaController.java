811ee2b1dafaa9d15b21435e30773498738acd24
FabrizioMisseri <misserifabrizio@gmail.com>
2023-04-04 09:01:35 +0200

    add service

--- src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -4,6 +4,7 @@ package org.learning.java.springlamiapizzeriacrud.controller;
 import jakarta.validation.Valid;
 import org.learning.java.springlamiapizzeriacrud.model.Pizza;
 import org.learning.java.springlamiapizzeriacrud.repository.PizzaRpository;
+import org.learning.java.springlamiapizzeriacrud.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -22,59 +23,87 @@ public class PizzaController {
     @Autowired
     private PizzaRpository pizzaRpository;
 
-    @GetMapping
-    public String index(Model model, @RequestParam(name = "q") Optional<String> keyword) {
+    @Autowired
+    private PizzaService pizzaService;
 
-//        List<Book> books;
+    //    @GetMapping
+//    public String index(Model model, @RequestParam(name = "q") Optional<String> keyword) {
+//
+//        List<Pizza> pizzas;
 //        if (keyword.isEmpty()) {
-//            books = bookRepository.findAll(Sort.by("title"));
+//            pizzas = pizzaRpository.findAll();
 //        } else {
-//            books = bookRepository.findByTitleContainingIgnoreCase(keyword.get());
+//            pizzas = pizzaRpository.findByNameContainingIgnoreCase(keyword.get());
 //            model.addAttribute("keyword", keyword.get());
 //        }
-//        model.addAttribute("list", books);
-
+//        model.addAttribute("allPizza", pizzas);
+//        return "/pizzas/index";
+//    }
+    @GetMapping
+    public String index(Model model, @RequestParam(name = "q") Optional<String> keyword) {
         List<Pizza> pizzas;
         if (keyword.isEmpty()) {
-            pizzas = pizzaRpository.findAll();
+            pizzas = pizzaService.getAllPizzas();
         } else {
-            pizzas = pizzaRpository.findByNameContainingIgnoreCase(keyword.get());
+            pizzas = pizzaService.getFilteredPizzas(keyword.get());
             model.addAttribute("keyword", keyword.get());
         }
         model.addAttribute("allPizza", pizzas);
+
         return "/pizzas/index";
     }
 
 
+    //    @GetMapping("/{id}")
+//    public String show(@PathVariable Integer id, Model model) {
+//        Pizza pizza = pizzaRpository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+//        model.addAttribute("pizza", pizza);
+//        return "/pizzas/show";
+//    }
     @GetMapping("/{id}")
     public String show(@PathVariable Integer id, Model model) {
-        Pizza pizza = pizzaRpository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        model.addAttribute("pizza", pizza);
-        return "/pizzas/show";
+        try {
+            Pizza pizza = pizzaService.getById(id);
+            model.addAttribute("pizza", pizza);
+            return "/pizzas/show";
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 
     // CREATE
     @GetMapping("/create")
     public String create(Model model) {
-
         model.addAttribute("pizza", new Pizza());
         return "/pizzas/create";
     }
 
     // STORE
+//    @PostMapping("/create")
+//    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+//
+//        if (bindingResult.hasErrors()) {
+//
+//            return "/pizzas/create";
+//        }
+//        Pizza pizzaToPersist = new Pizza();
+//        pizzaToPersist.setName(formPizza.getName());
+//        pizzaToPersist.setDescription(formPizza.getDescription());
+//        pizzaToPersist.setPrice(formPizza.getPrice());
+//        pizzaRpository.save(pizzaToPersist);
+//
+//        return "redirect:/pizzas";
+//    }
     @PostMapping("/create")
-    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
-
+    public String doCreate(@Valid @ModelAttribute("pizza") Pizza formPizza,
+                           BindingResult bindingResult, Model model) {
+        // VALIDATION
         if (bindingResult.hasErrors()) {
-
+            // ritorno alla view con il form
             return "/pizzas/create";
         }
-        Pizza pizzaToPersist = new Pizza();
-        pizzaToPersist.setName(formPizza.getName());
-        pizzaToPersist.setDescription(formPizza.getDescription());
-        pizzaToPersist.setPrice(formPizza.getPrice());
-        pizzaRpository.save(pizzaToPersist);
-
+        // se non ci sono errori procedo con la persistenza
+        pizzaService.createPizza(formPizza);
         return "redirect:/pizzas";
     }
 
