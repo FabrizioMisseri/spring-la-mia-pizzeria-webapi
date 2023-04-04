2413dec49e48ec2eb4ce6fb0065716ea4f49c933
FabrizioMisseri <misserifabrizio@gmail.com>
2023-04-04 15:22:59 +0200

    message-index

--- src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -133,10 +134,12 @@ public class PizzaController {
 
     // DELETE
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id) {
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
         try {
             boolean success = pizzaService.deleteById(id);
+
             if (success) {
+                redirectAttributes.addFlashAttribute("message", "la cancellazione è andata a buon fine");
                 return "redirect:/pizzas";
             } else {
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
