e807bf678462f0d0c7a1bc78c7877dc5d4894ea7
FabrizioMisseri <misserifabrizio@gmail.com>
2023-04-07 16:46:49 +0200

    *

--- src/main/java/org/learning/java/springlamiapizzeriacrud/security/DatabaseUserDetailService.java
+++ src/main/java/org/learning/java/springlamiapizzeriacrud/security/DatabaseUserDetailService.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Entity
+@Service
 public class DatabaseUserDetailService implements UserDetailsService {
-    
+
     @Autowired
     private UserRepository userRepository;
 
